5bc7c8e31102213f7310d3a20f0090da8e571f36
caiyijun <414065031@qq.com>
2018-04-21 00:02:33 +0800

    路由

---
+++ pages/about/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+      <h2>About Index Page</h2>
+      <ul>
+        <li><a href="/">HOME</a></li>
+      </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "index.vue"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/news/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h2>News Index Page</h2>
+    <p>NewsID:{{ $route.params.newsId }}</p>
+    <ul>
+      <li><a href="/">HOME</a></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "index"
+    }
+</script>
+
+<style scoped>
+
+</style>
